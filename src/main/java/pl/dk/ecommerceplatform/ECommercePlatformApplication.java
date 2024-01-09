88cdd0d9aef6ba63436c052d77a48049ddc2783e
Dawid Kolano <dawid.kolano@gmail.com>
2024-01-09 20:55:00 +0100

    Initial commit

---
+++ src/main/java/pl/dk/ecommerceplatform/ECommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package pl.dk.ecommerceplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommercePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommercePlatformApplication.class, args);
+    }
+
+}
