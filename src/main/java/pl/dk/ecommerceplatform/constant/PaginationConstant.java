38f2576f002934166987cf7a5ad119d26597a621
Dawid Kolano <dawid.kolano@gmail.com>
2024-01-20 13:50:09 +0100

    Added constant/PaginationConstant.class

---
+++ src/main/java/pl/dk/ecommerceplatform/constant/PaginationConstant.java
@@ -0,0 +1,10 @@
+package pl.dk.ecommerceplatform.constant;
+
+public class PaginationConstant {
+
+    public static final String PAGE_DEFAULT = "0";
+    public static final String SIZE_DEFAULT = "10";
+    public static final String ASC = "ASC";
+    public static final String DESC = "DESC";
+    public static final String SORT_NAME = "name";
+}
