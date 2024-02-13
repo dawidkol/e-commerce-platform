50eba088319c7e3ccc33000eb700d2a876acb4bd
Dawid Kolano <dawid.kolano@gmail.com>
2024-02-13 21:35:19 +0100

    Added tests for WarehouseController

---
+++ src/test/java/pl/dk/ecommerceplatform/warehouse/WarehouseControllerTest.java
@@ -0,0 +1,92 @@
+package pl.dk.ecommerceplatform.warehouse;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import pl.dk.ecommerceplatform.constant.PaginationConstant;
+import pl.dk.ecommerceplatform.warehouse.dtos.SaveItemDto;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static pl.dk.ecommerceplatform.constant.PaginationConstant.SIZE_DEFAULT;
+
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
+class WarehouseControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void init() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    @WithMockUser(username = "janusz.kowalski@test.pl", roles = "ADMIN")
+    void testWarehouseControllerFunctionality() throws Exception {
+        // 1. Test saving an item with invalid product ID
+        Long existingProductId = 1L;
+        Long itemQuantity = 10L;
+        boolean available = true;
+
+        SaveItemDto saveItemDtoWithInvalidProductId = SaveItemDto.builder()
+                .productId(existingProductId)
+                .quantity(itemQuantity)
+                .available(available)
+                .build();
+
+        String invalidJsonString = objectMapper.writeValueAsString(saveItemDtoWithInvalidProductId);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/items").content(invalidJsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isConflict());
+
+        // 2. Test saving an item with valid product ID
+        Long validProductId = 11L;
+
+        SaveItemDto saveItemDtoWithValidProductId = SaveItemDto.builder()
+                .productId(validProductId)
+                .quantity(itemQuantity)
+                .available(available)
+                .build();
+
+        String validJsonString = objectMapper.writeValueAsString(saveItemDtoWithValidProductId);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/items").content(validJsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+
+        // 3. Test updating the quantity of an existing item
+        Long itemId = 1L;
+        String content = """
+                {
+                    "quantity": 99
+                }
+                """;
+        mockMvc.perform(MockMvcRequestBuilders.patch("/items/{id}", itemId)
+                        .content(content).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+
+        // 4. Test retrieving all items with default paging and sorting
+        mockMvc.perform(MockMvcRequestBuilders.get("/items"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(jsonPath("$", hasSize(Integer.parseInt(SIZE_DEFAULT))));
+
+        // 5. Test deleting an item
+        mockMvc.perform(MockMvcRequestBuilders.delete("/items/{id}", itemId ))
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
+
+    }
+}
