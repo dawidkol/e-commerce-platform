89a00a82ce2dd8bc9b54b4fbf18224d5ac523c59
Dawid Kolano <dawid.kolano@gmail.com>
2024-02-18 16:34:58 +0100

    Refactor error handling in RestControllersErrorsHandler

--- src/main/java/pl/dk/ecommerceplatform/error/RestControllersErrorsHandler.java
+++ src/main/java/pl/dk/ecommerceplatform/error/RestControllersErrorsHandler.java
@@ -1,7 +1,6 @@
 package pl.dk.ecommerceplatform.error;
 
-import lombok.Builder;
-import org.hibernate.annotations.processing.SQL;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,23 +13,36 @@ import java.util.List;
 @RestControllerAdvice
 class RestControllersErrorsHandler {
 
-
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public List<ConstraintViolationError> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+    public List<MethodArgumentNotValidWrapper> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
         return ex.getBindingResult().getFieldErrors()
                 .stream()
-                .map(fieldError -> new ConstraintViolationError(fieldError.getField(), fieldError.getDefaultMessage()))
+                .map(fieldError -> new MethodArgumentNotValidWrapper(fieldError.getField(), fieldError.getDefaultMessage()))
                 .toList();
     }
 
-//    @ResponseStatus(HttpStatus.CONFLICT)
-//    @ExceptionHandler(SQLException.class)
-//    public SqlError handleDuplicateKeyViolates(SQLException ex) {
-//        return SqlError.builder()
-//                .error(ex.getMessage())
-//                .message("The user has already rated the product.")
-//                .build();
-//    }
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(SQLException.class)
+    public SqlErrorWrapper handleDuplicateKeyViolates(SQLException ex) {
+        return new SqlErrorWrapper(ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public List<ConstraintViolationWrapper> handleConstraintViolationException(ConstraintViolationException ex) {
+        return ex.getConstraintViolations()
+                .stream().map(fieldError -> new ConstraintViolationWrapper(fieldError.getInvalidValue().toString(), fieldError.getMessage()))
+                .toList();
+    }
+
+    record SqlErrorWrapper(String message) {
+    }
+
+    record MethodArgumentNotValidWrapper(String field, String message) {
+    }
+
+    record ConstraintViolationWrapper(String value, String message) {
+    }
 
 }
