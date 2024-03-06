94a3ba61f74b67abe6f9c5c6c4c6e1d688e7b73c
Dawid Kolano <dawid.kolano@gmail.com>
2024-03-06 20:47:39 +0100

    Replace TokenRepository method with custom native query.

--- src/main/java/pl/dk/ecommerceplatform/confirmationToken/TokenRepository.java
+++ src/main/java/pl/dk/ecommerceplatform/confirmationToken/TokenRepository.java
@@ -1,6 +1,7 @@
 package pl.dk.ecommerceplatform.confirmationToken;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +10,7 @@ import java.util.Optional;
 interface TokenRepository extends JpaRepository<Token, Long> {
 
     Optional<Token> findByToken(String token);
+
+    @Query(value = "SELECT * FROM token WHERE expiration > NOW()", nativeQuery = true)
     Optional<Token> findTokenByUser_Email(String userEmail);
 }
