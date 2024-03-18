9ff6e9d8c3977fbafa31c7d8bbd389e493b68e29
Dawid Kolano <dawid.kolano@gmail.com>
2024-03-18 21:24:50 +0100

    Refactor ImageNoFoundException to ImageNotFoundException.

--- src/main/java/pl/dk/ecommerceplatform/error/exceptions/productImage/ImageNotFoundException.java
+++ src/main/java/pl/dk/ecommerceplatform/error/exceptions/productImage/ImageNotFoundException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class ImageNoFoundException extends RuntimeException {
+public class ImageNotFoundException extends RuntimeException {
 
-    public ImageNoFoundException(String message) {
+    public ImageNotFoundException(String message) {
         super(message);
     }
 }
