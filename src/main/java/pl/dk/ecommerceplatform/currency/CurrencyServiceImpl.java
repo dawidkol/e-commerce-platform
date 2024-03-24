e632bd601a87129b687c657f4393bfc17b479e16
Dawid Kolano <dawid.kolano@gmail.com>
2024-03-24 16:31:33 +0100

    Refactored CurrencyService to CurrencyServiceImpl and added CurrencyService interface.

--- src/main/java/pl/dk/ecommerceplatform/currency/CurrencyServiceImpl.java
+++ src/main/java/pl/dk/ecommerceplatform/currency/CurrencyServiceImpl.java
@@ -3,15 +3,18 @@ package pl.dk.ecommerceplatform.currency;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.dk.ecommerceplatform.currency.dtos.CurrencyDto;
+import pl.dk.ecommerceplatform.error.exceptions.currency.CurrencyNotFoundException;
 
+import java.util.EnumSet;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
-class CurrencyService {
+class CurrencyServiceImpl implements CurrencyService {
 
     private final CurrencyRepository currencyRepository;
 
+    @Override
     public List<CurrencyDto> getAllCurrencies() {
         return currencyRepository.findAll()
                 .stream()
