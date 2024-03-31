3a45c52825a1bfdc23ce7268d5ca81db33131b6f
Dawid Kolano <dawid.kolano@gmail.com>
2024-03-31 19:26:22 +0200

    Added replyMessage field to Contact entity for storing response messages in replies.

--- src/main/java/pl/dk/ecommerceplatform/email/Contact.java
+++ src/main/java/pl/dk/ecommerceplatform/email/Contact.java
@@ -1,6 +1,10 @@
 package pl.dk.ecommerceplatform.email;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,10 +24,17 @@ class Contact {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Email
     private String email;
+    @NotBlank
     private String subject;
+    @NotBlank
+    @Size(min = 10, max = 10000)
     private String message;
+    @NotNull
     private LocalDateTime dateOfPosting;
     private LocalDateTime replyDate;
+    @Size(min = 10, max = 10000)
+    private String replyMessage;
 
 }
