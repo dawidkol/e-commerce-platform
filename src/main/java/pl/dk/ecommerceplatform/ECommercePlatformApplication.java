dce32646c2c8ac4ef33c350f295ebdefb4679e15
Dawid Kolano <dawid.kolano@gmail.com>
2024-04-18 21:17:57 +0200

    Added TimeZone setting in main class.

--- src/main/java/pl/dk/ecommerceplatform/ECommercePlatformApplication.java
+++ src/main/java/pl/dk/ecommerceplatform/ECommercePlatformApplication.java
@@ -1,12 +1,17 @@
 package pl.dk.ecommerceplatform;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
+@RequiredArgsConstructor
 public class ECommercePlatformApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Warsaw"));
         SpringApplication.run(ECommercePlatformApplication.class, args);
     }
 
